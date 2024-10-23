48b6f5851a98da92b6f37b704e60ed6607cee3a1
Konstantinos Pantos <Konstantinos.Pantos@microsoft.com>
2024-10-23 13:41:18 +0300

    Initial Commit

---
+++ setup.ps1
@@ -0,0 +1,301 @@
+## Basic Dev Utilities Section
+
+Write-Output Install Microsoft.DotNet.SDK.3_1
+winget install -e --id Microsoft.DotNet.SDK.3_1 --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.DotNet.SDK.3_1 installed successfully.} else { Write-Output Error installing Microsoft.DotNet.SDK.3_1!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.DotNet.SDK.5
+winget install -e --id Microsoft.DotNet.SDK.5 --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.DotNet.SDK.5 installed successfully.} else { Write-Output Error installing Microsoft.DotNet.SDK.5!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.DotNet.SDK.6
+winget install -e --id Microsoft.DotNet.SDK.6 --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.DotNet.SDK.6 installed successfully.} else { Write-Output Error installing Microsoft.DotNet.SDK.6!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.DotNet.SDK.7
+winget install -e --id Microsoft.DotNet.SDK.7 --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.DotNet.SDK.7 installed successfully.} else { Write-Output Error installing Microsoft.DotNet.SDK.7!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.DotNet.SDK.8
+winget install -e --id Microsoft.DotNet.SDK.8 --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.DotNet.SDK.8 installed successfully.} else { Write-Output Error installing Microsoft.DotNet.SDK.8!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Total Commander
+winget install --id=Ghisler.TotalCommander -e --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output "Total Commander installed successfully." } else { Write-Output "Error installing Total Commander" }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.WindowsTerminal
+winget install --id=Microsoft.WindowsTerminal --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output "Microsoft.WindowsTerminal installed successfully." } else { Write-Output "Error installing Microsoft.WindowsTerminal!!!" }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.Powertoys
+winget install --id=Microsoft.Powertoys --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.Powertoys installed successfully.} else { Write-Output Error installing Microsoft.Powertoys!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.Powershell
+winget install --id=Microsoft.Powershell --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.Powershell installed successfully.} else { Write-Output Error installing Microsoft.Powershell!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.AzureCLI
+winget install --id=Microsoft.AzureCLI --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.AzureCLI installed successfully.} else { Write-Output Error installing Microsoft.AzureCLI!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.Bicep
+winget install --id=Microsoft.Bicep --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.Bicep installed successfully.} else { Write-Output Error installing Microsoft.Bicep!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.Azd
+winget install --id=Microsoft.Azd --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.Azd installed successfully.} else { Write-Output Error installing Microsoft.Azd!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.AzureDataStudio
+winget install --id=Microsoft.AzureDataStudio --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.AzureDataStudio installed successfully.} else { Write-Output Error installing Microsoft.AzureDataStudio!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.AzureFunctionsCoreTools
+winget install --id=Microsoft.AzureFunctionsCoreTools --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.AzureFunctionsCoreTools installed successfully.} else { Write-Output Error installing Microsoft.AzureFunctionsCoreTools!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Kubernetes.kubectl
+winget install --id=Kubernetes.kubectl --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Kubernetes.kubectl installed successfully.} else { Write-Output Error installing Kubernetes.kubectl!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install RedHat.Podman
+winget install --id=RedHat.Podman --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output RedHat.Podman installed successfully.} else { Write-Output Error installing RedHat.Podman!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Git.Git
+winget install --id=Git.Git --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Git.Git installed successfully.} else { Write-Output Error installing Git.Git!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Github.cli
+winget install --id=Github.cli --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Github.cli installed successfully.} else { Write-Output Error installing Github.cli!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Docker.DockerDesktop
+winget install --id=Docker.DockerDesktop --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Docker.DockerDesktop installed successfully.} else { Write-Output Error installing Docker.DockerDesktop!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.VisualStudioCode
+winget install --id=Microsoft.VisualStudioCode --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.VisualStudioCode installed successfully.} else { Write-Output Error installing Microsoft.VisualStudioCode!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Storage Emulator
+winget install -e --id Microsoft.Azure.StorageEmulator --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Storage Emulator installed successfully.} else { Write-Output Error installing Storage Emulator!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.AzureStorageExplorer
+winget install -e --id Microsoft.Azure.StorageExplorer --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.AzureStorageExplorer installed successfully.} else { Write-Output Error installing Microsoft.AzureStorageExplorer!!! }
+Write-Output * * * * * * * * * *
+
+# Write-Output Install ServiceFabricSDK
+# winget install -e --id Microsoft.ServiceFabricRuntime --accept-package-agreements --accept-source-agreements
+# winget install -e --id Microsoft.ServiceFabricExplorer --accept-package-agreements --accept-source-agreements
+# if ($LASTEXITCODE -eq 0) { Write-Output ServiceFabricSDK installed successfully.} else { Write-Output Error installing ServiceFabricSDK!!! }   
+# Write-Output * * * * * * * * * *
+
+# Write-Output Install Microsoft.SQLServer
+# winget install -e --id Microsoft.SQLServerManagementStudio --accept-package-agreements --accept-source-agreements
+# winget install -e --id Microsoft.AzureDataStudio --accept-package-agreements --accept-source-agreements
+# winget install -e --id Microsoft.SQLServer.2019.Developer --accept-package-agreements --accept-source-agreements
+# if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.SQLServer installed successfully.} else { Write-Output Error installing Microsoft.SQLServer!!! }
+# Write-Output * * * * * * * * * *
+
+Write-Output Install Insomnia
+winget install -e --id Insomnia.Insomnia --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Insomnia installed successfully.} else { Write-Output Error installing Insomnia!!! }
+Write-Output * * * * * * * * * *
+
+# Write-Output Install VisualStudio
+# winget install -e --id Microsoft.VisualStudio.2022.Enterprise.Preview --accept-package-agreements --accept-source-agreements
+# if ($LASTEXITCODE -eq 0) { Write-Output VisualStudio installed successfully.} else { Write-Output Error installing VisualStudio!!! }
+# Write-Output * * * * * * * * * *
+
+Write-Output Install Lens
+winget install -e --id Mirantis.Lens --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Lens installed successfully.} else { Write-Output Error installing Lens!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.BotFrameworkComposer
+winget install -e --id Microsoft.BotFrameworkComposer --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.BotFrameworkComposer installed successfully.} else { Write-Output Error installing Microsoft.BotFrameworkComposer!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install PostgreSQL.pgAdmin
+winget install -e --id PostgreSQL.pgAdmin --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output PostgreSQL.pgAdmin installed successfully.} else { Write-Output Error installing PostgreSQL.pgAdmin!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.AzureMediaServicesExplorer
+winget install -e --id Microsoft.AzureMediaServicesExplorer --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.AzureMediaServicesExplorer installed successfully.} else { Write-Output Error installing Microsoft.AzureMediaServicesExplorer!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.Azure.CosmosEmulator
+winget install -e --id Microsoft.Azure.CosmosEmulator --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.Azure.CosmosEmulator installed successfully.} else { Write-Output Error installing Microsoft.Azure.CosmosEmulator!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.webpicmd 
+winget install -e --id Microsoft.webpicmd --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.webpicmd installed successfully.} else { Write-Output Error installing Microsoft.webpicmd!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Microsoft.RemoteDesktopClient
+winget install -e --id Microsoft.RemoteDesktopClient --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Microsoft.RemoteDesktopClient installed successfully.} else { Write-Output Error installing Microsoft.RemoteDesktopClient!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Telerik.Fiddler.Classic
+winget install -e --id Telerik.Fiddler.Classic --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Telerik.Fiddler.Classic installed successfully.} else { Write-Output Error installing Telerik.Fiddler.Classic!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install GitHub.GitHubDesktop
+winget install -e --id GitHub.GitHubDesktop --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output GitHub.GitHubDesktop installed successfully.} else { Write-Output Error installing GitHub.GitHubDesktop!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Axosoft.GitKraken
+winget install -e --id Axosoft.GitKraken --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Axosoft.GitKraken installed successfully.} else { Write-Output Error installing Axosoft.GitKraken!!! }
+Write-Output * * * * * * * * * *
+
+## Misc Utilities Section
+
+Write-Output Install AgileBits.1Password
+winget install -e --id AgileBits.1Password --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output AgileBits.1Password installed successfully.} else { Write-Output Error installing AgileBits.1Password!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Termius.Termius
+winget install -e --id Termius.Termius --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Termius.Termius installed successfully.} else { Write-Output Error installing Termius.Termius!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install OpenVPNTechnologies.OpenVPNConnect
+winget install -e --id OpenVPNTechnologies.OpenVPNConnect --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output OpenVPNTechnologies.OpenVPNConnect installed successfully.} else { Write-Output Error installing OpenVPNTechnologies.OpenVPNConnect!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install PuTTY.PuTTY
+winget install -e --id PuTTY.PuTTY --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output PuTTY.PuTTY installed successfully.} else { Write-Output Error installing PuTTY.PuTTY!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install dotPDN.PaintDotNet
+winget install -e --id dotPDN.PaintDotNet --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output dotPDN.PaintDotNet installed successfully.} else { Write-Output Error installing dotPDN.PaintDotNet!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Notepad++.Notepad++
+winget install --id=Notepad++.Notepad++ --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Notepad++.Notepad++ installed successfully.} else { Write-Output Error installing Notepad++.Notepad++!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install WinMerge.WinMerge
+winget install --id=WinMerge.WinMerge --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output WinMerge.WinMerge installed successfully.} else { Write-Output Error installing WinMerge.WinMerge!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install IrfanSkiljan.IrfanView
+winget install --id=IrfanSkiljan.IrfanView --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output IrfanSkiljan.IrfanView installed successfully.} else { Write-Output Error installing IrfanSkiljan.IrfanView!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Gimp.Gimp
+winget install --id=Gimp.Gimp --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Gimp.Gimp installed successfully.} else { Write-Output Error installing Gimp.Gimp!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Cyanfish.NAPS2
+winget install --id=Cyanfish.NAPS2 --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Cyanfish.NAPS2 installed successfully.} else { Write-Output Error installing Cyanfish.NAPS2!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install VideoLAN.VLC
+winget install --id=VideoLAN.VLC --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output VideoLAN.VLC installed successfully.} else { Write-Output Error installing VideoLAN.VLC!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Viber.Viber
+winget install --id=Viber.Viber --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Viber.Viber installed successfully.} else { Write-Output Error installing Viber.Viber!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Telegram.TelegramDesktop
+winget install --id=Telegram.TelegramDesktop --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Telegram.TelegramDesktop installed successfully.} else { Write-Output Error installing Telegram.TelegramDesktop!!! }   
+Write-Output * * * * * * * * * *
+
+Write-Output Install WhatsApp.WhatsApp
+winget install --id=WhatsApp.WhatsApp --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output WhatsApp.WhatsApp installed successfully.} else { Write-Output Error installing WhatsApp.WhatsApp!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install FFmpeg
+winget install --id=Gyan.FFmpeg --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output FFmpeg installed successfully.} else { Write-Output Error installing FFmpeg!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output Install Firefox
+winget install -e --id Mozilla.Firefox.DeveloperEdition --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Firefox installed successfully.} else { Write-Output Error installing Firefox!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output install Acrobat Reader
+winget install -e --id Adobe.Acrobat.Reader --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Adobe.Acrobat.Reader installed successfully.} else { Write-Output Error installing Adobe.Acrobat.Reader!!! }
+Write-Output * * * * * * * * * *
+
+Write-Output install draw.io
+winget install -e --id JGraph.Draw --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output JGraph.Draw installed successfully.} else { Write-Output Error installing JGraph.Draw!!! }
+write-output * * * * * * * * * *
+
+Write-Output install Inkscape.Inkscape
+winget install -e --id Inkscape.Inkscape --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Inkscape.Inkscape installed successfully.} else { Write-Output Error installing Inkscape.Inkscape!!! }
+write-output * * * * * * * * * *
+
+Write-Output install OBSProject.OBSStudio
+winget install -e --id OBSProject.OBSStudio --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output OBSProject.OBSStudio installed successfully.} else { Write-Output Error installing OBSProject.OBSStudio!!! }
+write-output * * * * * * * * * *
+
+Write-Output install Audacity
+winget install -e --id Audacity.Audacity --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output Audacity.Audacity installed successfully.} else { Write-Output Error installing Audacity.Audacity!!! }
+write-output * * * * * * * * * *
+
+Write-Output install WinRAR
+winget install -e --id RARLab.WinRAR --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output RARLab.WinRAR installed successfully.} else { Write-Output Error installing RARLab.WinRAR!!! }
+write-output * * * * * * * * * *
+
+Write-Output install WinSCP.WinSCP
+winget install -e --id WinSCP.WinSCP --accept-package-agreements --accept-source-agreements
+if ($LASTEXITCODE -eq 0) { Write-Output WinSCP.WinSCP installed successfully.} else { Write-Output Error installing WinSCP.WinSCP!!! }
+write-output * * * * * * * * * *
